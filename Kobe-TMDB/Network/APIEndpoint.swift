f38371bc083cbb3e91e96dad47a4c42bc991f64a
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2019-07-24 01:10:51 -0300

    [FEATURE]: Added movie search endpoint

--- Kobe-TMDB/Network/APIEndpoint.swift
+++ Kobe-TMDB/Network/APIEndpoint.swift
@@ -12,6 +12,7 @@ enum APIEndpoint {
     case upcoming(language: String, page: Int)
     case genre
     case movieDetail(_ id: Int, language: API.Language)
+    case searchMovie(query: String, language: String, page: Int)
 }
 
 extension APIEndpoint {
@@ -25,6 +26,8 @@ extension APIEndpoint {
             return "genre/movie/list"
         case .movieDetail(let id, _):
             return "movie/\(id)"
+        case .searchMovie:
+            return "search/movie"
         }
     }
     
@@ -32,10 +35,11 @@ extension APIEndpoint {
         
         switch self {
         case .upcoming(let language, let page):
-            return ["language": language,
-                    "page": "\(page)"]
-        case.movieDetail(_ ,let language):
+            return ["language": language, "page": "\(page)"]
+        case .movieDetail(_ ,let language):
             return ["language": language.type]
+        case .searchMovie(let query, let language, let page):
+            return ["query": query, "language": language, "page": "\(page)", "include_adult": "false"]
         default:
             return [:]
         }
